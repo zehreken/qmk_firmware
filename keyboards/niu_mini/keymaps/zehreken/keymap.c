86edcad2b2574a579f8140d2c01cf23ae999e30b
zehreken <zehreken@gmail.com>
2019-08-23 20:13:32 +0200

    Update layout

--- keyboards/niu_mini/keymaps/zehreken/keymap.c
+++ keyboards/niu_mini/keymaps/zehreken/keymap.c
@@ -14,10 +14,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[0] = LAYOUT_planck_mit(
-		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-		KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+		KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, TO(0),     KC_SPC,     TO(2), KC_LEFT, KC_RGHT, KC_UP,   KC_DOWN
+		_______, KC_LCTL, KC_LALT, KC_LGUI, TO(0),     KC_SPC,     TO(2), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
   ),
 
 	/* Layer 1
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
 		_______, _______, _______, _______, _______, _______, _______, KC_F11,  KC_F12,  _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		RESET,   _______, _______, _______, _______,     _______,      _______, _______, KC_MUTE, KC_VOLU, KC_VOLD
+		RESET,   _______, _______, _______, _______,     _______,      _______, _______, KC_VOLD, KC_VOLU, KC_MUTE
 	),
 
 	/* Layer 2 (r_ Indicates RGB Controls)
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[2] = LAYOUT_planck_mit(
-		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-		_______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______, _______,
+		KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+		KC_GRV,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______, _______,
 		_______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  _______, _______, _______, _______,
-		RESET,   _______, _______, _______, _______,     _______,      _______, _______, KC_MUTE, KC_VOLU, KC_VOLD
+		RESET,   _______, _______, _______, _______,     _______,      _______, _______, KC_VOLD, KC_VOLU, KC_MUTE
   )
 };
 
